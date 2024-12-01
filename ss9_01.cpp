98fadbe973e7b1f418cbcaa105b392a055823089
giahung123456 <dogiahungtplc@gmail.com>
2024-12-01 23:22:20 +0700

    UPLOAD SS9_01

---
+++ ss9_01.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+  main(){
+  	int ara[100],n,s;
+  	printf("so phan tu muon co:");
+  	scanf("%d",&n);
+  	
+  	printf("vi tri:");
+  	scanf("%d",&s);
+  	for(int i=0;i<n;i++){	
+  		printf("ara[%d]=",i);
+  		scanf("%d",&ara[i]);
+  		
+	  	}
+	  	 	for(int i=0;i<n;i++){
+		  printf("%3d",ara[i]);
+	}
+	//for(int i=0;i<n;i++);   
+	   
+	  	}
